671706ac14cad523f832631bc96ab6a70187b075
umesh375 <yedakeu@gmail.com>
2024-11-02 11:21:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function daysofAYear(year) {
+	const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+	return isLeapYear ? 366 : 365;
+}
+
+// Examples
+console.log(daysofAYear(2022)); //365
+console.log(daysofAYear(2024)); //365
+console.log(daysofAYear(1900)); //365
+console.log(daysofAYear(2000)); //365
